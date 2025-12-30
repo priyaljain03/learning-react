3bd1fb18ca0ab726ad8d23bb4285f6d8ff925def
Priyal Jain <jainpriyal03@gmail.com>
2025-12-30 12:56:53 +0530

    Initial commit

---
+++ App.js
@@ -0,0 +1,4 @@
+const heading = React.createElement("h1", {}, "Hello world from React");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+console.log(root);
+root.render(heading);
