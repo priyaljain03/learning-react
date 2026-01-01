4d6d9a03e21dac98e4b50dbbcdcf5b88b10a39d3
Priyal Jain <jainpriyal03@gmail.com>
2026-01-02 00:04:19 +0530

    Episode 2 notes and code

--- App.js
+++ App.js
@@ -1,4 +1,19 @@
-const heading = React.createElement("h1", {}, "Hello world from React");
+/**
+ * <div class="parent">
+ *  <div class="child">
+ *    <h1>I am h1 tag</h1>
+ *    <h2>I am h2 tag</h2>
+ *  </div>
+ *  <div class="child2">
+ *    <h1>I am h1 tag</h1>
+ *    <h2>I am h2 tag</h2>
+ *  </div>
+ * </div>
+ */
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement("div", { id: "parent" } ,   React.createElement("div", {id: "child"} , React.createElement("h1", {}, "I am h1 tag")), React.createElement("div", {id: "child2"} , React.createElement("h1", {}, "I am h1 tag"), React.createElement("h2", {}, "I am h2 tag")));
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-console.log(root);
-root.render(heading);
+root.render(parent);
