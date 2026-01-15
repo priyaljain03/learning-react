e07f81ac6698fe7c3bbf62438df00536755ddaa1
priyaljain03 <38190378+priyaljain03@users.noreply.github.com>
2026-01-15 22:58:24 +0530

    Added code to App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import About from "./components/About";
 
+const AppLayout = () => {
+    return <div className="app" >
+        <Header />
+        <Body />
+    </div>
+}
+
+const appRouter = createBrowserRouter([
+    {
+        path: "/",
+        element: <AppLayout />,
+    },
+    {
+        path: "/about",
+        element: <About />,
+    }
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={appRouter} />);
